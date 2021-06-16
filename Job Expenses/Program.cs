24756e2a24264c0b40e909998cd7fff78c625d17
Varsha Issrani <vissrani@usf.edu>
2021-06-16 13:45:28 -0400

    Add project files.

---
+++ Job Expenses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Job_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = int.Parse(Console.ReadLine());
+            int totalexpenses = 0;
+
+            string input = Console.ReadLine();
+
+            string[] incomeandexpenses = input.Split(" ");
+
+            List<string> allexpenses = new List<string>();
+
+
+            foreach (var incexp in incomeandexpenses)
+            {
+                if (incexp.Contains('-'))
+                {
+                    allexpenses.Add(incexp);
+                }
+            }
+
+            foreach (var expense in allexpenses)
+            {
+                totalexpenses += int.Parse(expense);
+            }
+
+            Console.WriteLine(Math.Abs(totalexpenses));
+        }
+    }
+}
